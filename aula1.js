3d76bbc4c597501ed15ef4332e8a6102c9d7d675
poca753 <165678402+poca753@users.noreply.github.com>
2026-02-23 08:21:14 -0300

    643
    5345

---
+++ aula1.js
@@ -0,0 +1,8 @@
+//Em JavaScript tem 3 tipos de dados; o booleano, numerico e a string
+//O numerico e chamado de number
+//Os dados são armazenados em constantes ou variaveis
+const meuNUMERO = 3;
+const primeiroNUMERO = 7;
+const segundoNUMERO = 12;
+const operacao = primeiroNUMERO + segundoNUMERO
+console.log(operacao)
